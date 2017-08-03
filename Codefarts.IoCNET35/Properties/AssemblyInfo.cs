3aeb451361c48bd8d8f0b30e377983f4f768760e
Dean Lunz <createdbyx@createdbyx.com>
2017-08-03 10:11:34 -0700

    project setup

--- Codefarts.IoCNET35/Properties/AssemblyInfo.cs
+++ Codefarts.IoCNET35/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("503e8acc-ec61-4338-b4e5-49c6f8ca894c")]
+[assembly: Guid("962d2f24-712b-440d-9a0c-e86905210ac1")]
 
 // Version information for an assembly consists of the following four values:
 //
