a41fe4ca7bd6b05e95334b018d9ce3ae9d8a83fa
Dean Lunz <createdbyx@createdbyx.com>
2022-11-11 02:24:28 -0800

    added benchmarks

--- Codefarts.IoC.Benchmarking/Program.cs
+++ Codefarts.IoC.Benchmarking/Program.cs
@@ -2,18 +2,66 @@
 
 using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 using Codefarts.IoC;
+using Codefarts.IoC.Tests;
 
-BenchmarkRunner.Run<Benchmarks>();
+namespace Codefarts.IoC.Benchmarking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
 
 public class Benchmarks
 {
+    private const int IterationCount = 10000;
+    //    private Container container;
+    //
+    // [GlobalSetup]
+    // public void IterationSetup()
+    // {
+    //     this.container = new Container();
+    // }
+    //
+    // [GlobalCleanup]
+    // public void IterationCleanup()
+    // {
+    //     this.container = null;
+    // }
+
     [Benchmark]
-    public void Resolve()
+    public void ResolveStopWatch()
     {
         var container = new Container();
-        var value = container.Resolve<Stopwatch>();
+        for (var i = 0; i < IterationCount; i++)
+        {
+            container.Resolve<Stopwatch>();
+        }
+    }
+
+    [Benchmark]
+    public void ResolveICollectionArrayDependencyWithRegistration()
+    {
+        var container = new Container();
+        container.Register<ICollection<ITestInterface>, ITestInterface[]>();
+        for (var i = 0; i < IterationCount; i++)
+        {
+            container.Resolve<TestClassEnumerableDependency>();
+        }
+    }
+
+    [Benchmark]
+    public void ResolveIEnumerableDependencyWithoutRegistration()
+    {
+        var container = new Container();
+        for (var i = 0; i < IterationCount; i++)
+        {
+            container.Resolve<TestClassEnumerableDependency>();
+        }
     }
 }
